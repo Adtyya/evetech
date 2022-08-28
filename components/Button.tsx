4a090f60ac6f788d76b3aff5df4e4959f8ba91a5
Aditya <aditdualapan@gmail.com>
2022-08-28 19:04:59 +0700

    add countdown

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,19 @@
-import React, { FC } from "react";
+import React, { ButtonHTMLAttributes, FC } from "react";
 
 interface Component {
   children: String;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  type?: "submit" | "reset" | "button";
 }
 
-const Button: FC<Component> = ({ children, onClick }) => {
+const Button: FC<Component> = ({ children, onClick, type }) => {
   console.log("render");
   return (
     <>
       <button
         className="bg-slate-900 opacity-50 px-4 py-1 text-white rounded"
         onClick={onClick}
+        type={type}
       >
         {children}
       </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect, useCallback, ChangeEvent } from "react";
 import Button from "../components/Button";
 
 const index = () => {
   const [count, setCount] = useState<number>(0);
+  const [countDown, setCountDown] = useState<null | number>(null);
+  const [start, setStart] = useState<boolean>(false);
 
   const Increment = useCallback(() => {
     setCount((prev) => prev + 1);
@@ -12,14 +14,63 @@ const index = () => {
     setCount((prev) => prev - 1);
   }, [setCount]);
 
+  const handleCountDown = (event: ChangeEvent<HTMLInputElement>) => {
+    const val = event.target.value as unknown as number;
+    setCountDown(val ? val : null);
+  };
+
+  const startCountDown = (event: any) => {
+    event.preventDefault();
+    setStart(true);
+  };
+
+  useEffect(() => {
+    let interval: ReturnType<typeof setInterval>;
+    if (start) {
+      interval = setInterval(() => {
+        setCountDown(countDown ? countDown - 1 : countDown);
+        if (countDown === 1) {
+          setStart(false);
+          setCountDown(null);
+          alert("Waktu habis!");
+        }
+      }, 1000);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [start, countDown]);
+
   return (
-    <div className="flex flex-col w-full h-screen items-center justify-center bg-slate-900">
+    <div className="flex flex-col w-full h-screen items-center justify-start bg-slate-900 p-5 space-y-5">
       <div className="flex flex-col justify-center items-center space-y-5 w-3/4 h-auto px-4 py-5 bg-slate-200 text-gray-500 bg-opacity-10 rounded">
         <h1 className="text-4xl">Jumlah : {count}</h1>
         <div className="flex space-x-4">
           <Button onClick={Increment}>Tambah Nilai</Button>
           <Button onClick={Decrement}>Kurangi Nilai</Button>
         </div>
+      </div>
+      <div className="flex flex-col justify-center items-center w-3/4 h-auto px-4 py-5 bg-slate-200 text-gray-500 bg-opacity-10 rounded">
+        <form className="text-center space-y-5" onSubmit={startCountDown}>
+          <h1 className="text-4xl">Masukan waktu countdown dalam detik</h1>
+          <input
+            type="number"
+            className="content-center focus:outline focus:outline-slate-300 rounded py-1 text-center mx-3"
+            placeholder="Masukan hanya angka"
+            onChange={handleCountDown}
+            value={countDown ? countDown : ""}
+          />
+          <Button type="submit">Mulai</Button>
+        </form>
+        {countDown === null ? (
+          <p className="text-gray-500 text-2xl py-3">
+            Waktu anda telah habis! Silahkan isi kembali .
+          </p>
+        ) : (
+          <p className="text-gray-500 text-2xl py-3">
+            Waktu anda tersisa {countDown} detik.
+          </p>
+        )}
       </div>
     </div>
   );
